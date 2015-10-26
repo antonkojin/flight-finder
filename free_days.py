73f4fffc952b67a3990d379f3c5c12cf283f56cc
anton kozhin <antonkojin@gmail.com>
2015-10-26 07:57:56 +0100

    added Searcher constructor in free_days.py

--- free_days.py
+++ free_days.py
@@ -3,3 +3,18 @@
 
 import logging
 log = logging.getLogger(__name__)
+
+
+class Searcher:
+    def __init__(self, config):
+        self.config = config
+        from utils import StringsToDates
+        self.freeDays = StringsToDates(config["free"])
+        log.debug("free days: {}".format(self.freeDays))
+        from utils import ReadJson
+        flights = ReadJson(self.config['db'])
+        self.on_db = flights['on']
+        self.back_db = flights['back']
+
+    def searchFreeDaysFlights(self):
+        pass
